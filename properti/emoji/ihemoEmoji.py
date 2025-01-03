bc4142c9cedf473dfae3f9d1fc2ab88e49fd8bed
Hv303 <144507182+Hv303@users.noreply.github.com>
2025-01-03 21:25:37 +0700

    Add emoji

---
+++ properti/emoji/ihemoEmoji.py
@@ -0,0 +1,16 @@
+# Dont touch
+null = ("")
+# Surg - by ihemo 
+SurgicalAnesthetic    = "<:SurgicalAnesthetic:892136763718512642>"          
+SurgicalAntibiotics   = "<:SurgicalAntibiotics:892136764045684787>"      
+SurgicalAntiseptic    = "<:SurgicalAntiseptic:892136764058255431>"          
+SurgicalDefibrillator = "<:SurgicalDefibrillator:892136728511533076>"     
+SurgicalLabKit        = "<:SurgicalLabKit:892136728566071367>"                 
+SurgicalLoveMallet    = "<:SurgicalLoveMallet:892136728566050836>"   
+SurgicalPins          = "<:SurgicalPins:892136728566063104>"                 
+SurgicalScalpel       = "<:SurgicalScalpel:958337498742935552>"             
+SurgicalSplint        = "<:SurgicalSplint:892136728645746688>"                
+SurgicalSponge        = "<:SurgicalSponge:892136728591216700>"               
+SurgicalStitches      = "<:SurgicalStitches:892136728607985734>"             
+SurgicalTransfusion   = "<:SurgicalTransfusion:892136795712659516>"        
+SurgicalUltrasound    = "<:SurgicalUltrasound:892136795570065489>"      
